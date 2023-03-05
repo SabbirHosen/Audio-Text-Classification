8dbdff9ce60b18c5e88fe722944aa16bf5fd7c75
Sabbir Hosen <sabbircse44@gmail.com>
2023-03-05 16:03:39 +0600

    audio to emotion classification done

--- emotionDetection/views.py
+++ emotionDetection/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse, redirect
-from .analysis import text_to_emotion
+from .analysis import text_to_emotion, get_large_audio_transcription
+from .models import UploadFile
 
 
 # Create your views here.
@@ -11,6 +12,7 @@ def home(request):
 
 
 def text_input(request):
+    print(request)
     if request.method == "POST":
         input_text = request.POST.get('text')
         results = text_to_emotion(input_text)
@@ -22,6 +24,38 @@ def text_input(request):
             'top': results[0]
         }
         # print(data)
+        return render(request, 'index.html', context=data)
+        # return redirect('emotionDetection:home')
+    return redirect('emotionDetection:home')
+
+
+def audio_input(request):
+    # print(request)
+    if request.method == "POST":
+        input_text = request.FILES.get('video-file-upload')
+        # print(input_text)
+        file_obj = UploadFile()
+        file_obj.file = input_text
+        file_obj.save()
+        #
+        new_f = UploadFile.objects.last()
+        add_url = str(new_f.file.url)
+        print(f'E:/NSU Project/project/audioTextClassification/{add_url[1:]}')
+        path = f'E:/NSU Project/project/audioTextClassification/{add_url[1:]}'
+        # print()
+        # sound = AudioSegment.from_mp3(add_url[1:])
+        # print('')
+        # with open('../media/audio/death.mp3') as o
+        text = get_large_audio_transcription(path)
+        results = text_to_emotion(text)
+        data = {
+            'title': 'Result of Emotion Detection',
+            'results': results,
+            'text': text,
+            'top': results[0]
+        }
+        # print(data)
+        new_f.delete()
         return render(request, 'index.html', context=data)
         # return redirect('emotionDetection:home')
     return redirect('emotionDetection:home')
